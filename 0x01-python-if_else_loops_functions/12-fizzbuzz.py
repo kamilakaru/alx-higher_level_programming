343a11ffd9515c962c429f548b90ba09e2b55c0b
kamilakaru <karukamila@gmail.com>
2022-06-06 15:54:23 +0300

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 
 def fizzbuzz():
     """
-    FIZZ BUZZ
+    Fizz Buzz
     """
     for i in range(1, 101):
-        if 1 % 15 == 0:
+        if i % 15 == 0:
             print("FizzBuzz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
