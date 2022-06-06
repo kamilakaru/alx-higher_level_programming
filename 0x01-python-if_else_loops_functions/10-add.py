b64439e5d364200208444090c96838b8b529e390
kamilakaru <karukamila@gmail.com>
2022-06-06 15:35:40 +0300

    a + b

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Adds two integers
+    Arguments:
+        a: integer
+        b: interger
+    Return:
+        sum of a and b
+    """
+    return a + b
