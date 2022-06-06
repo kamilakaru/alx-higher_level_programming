fb5bcc11f77b9006ea19048c807b393a2ffe9e53
kamilakaru <karukamila@gmail.com>
2022-06-06 21:17:32 +0300

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -16,4 +16,4 @@ def new_in_list(my_list, idx, element):
     if (idx < 0 or idx >= len(my_list)):
         return my_list[:]
 
-    return my_list[:idx] + [element] + my_list[idx + 1:] 
+    return my_list[:idx] + [element] + my_list[idx + 1:]
