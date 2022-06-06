b93783fbf2cfcc0e7c2648dbfb7f7a5039c0add3
kamilakaru <karukamila@gmail.com>
2022-06-06 15:40:21 +0300

    pow

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    computes a raised to the power of  b
+    Arguments:
+        a: interger
+        b: interger
+    Return:
+        The value of a ^ b
+    """
+    return a ** b
+
