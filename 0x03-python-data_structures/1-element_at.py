e35ddceaf558723a4b02de6815e04df2aae26231
kamilakaru <karukamila@gmail.com>
2022-06-06 20:27:02 +0300

    element

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """
+    Retrieves an alement from a list
+
+    Args:
+        my_list: list of integers
+        idx: integer
+
+    Return:
+        Element at position idx in my_list. Otherwise None
+    """
+    return None if (idx < 0 or idx >= len(my_list)) else my_list[idx]
