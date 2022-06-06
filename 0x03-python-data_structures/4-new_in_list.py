c53521fad7e3262b48bbc5883768ac92337e0408
kamilakaru <karukamila@gmail.com>
2022-06-06 21:15:17 +0300

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """
+    Replaces an element in a list at a specific position
+    witout modifying the original list
+
+    Args:
+        my_list: list of integers
+        idx: integers
+        element: integers
+
+    Return:
+        list of intergers
+    """
+    if (idx < 0 or idx >= len(my_list)):
+        return my_list[:]
+
+    return my_list[:idx] + [element] + my_list[idx + 1:] 
