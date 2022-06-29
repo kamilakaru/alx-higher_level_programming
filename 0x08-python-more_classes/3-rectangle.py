0718c54954887ffb3b5e679fee6cdc4aa636a4aa
kamilakaru <karukamila@gmail.com>
2022-06-29 17:10:11 +0300

    3-add

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -49,16 +49,17 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        """returns the perimeter of the rectangle"""
+        """returns the perimeter of the 
+        rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width * 2) + (self.__height * 2)
 
     def __str__(self):
-        """returns printable string
+        """returns printable string 
         representation of the rectangle"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width 
-                    for j in range(self.__height))
-            return string
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
+        return string
